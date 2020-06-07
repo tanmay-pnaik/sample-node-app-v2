3ff42d5c1b16dd63f2433ab046ad7eb91eae0267
Gunjan Garge <gunjangarge@gmail.com>
2020-06-07 19:51:55 +0530

    Update server.js

--- source/server.js
+++ source/server.js
@@ -5,10 +5,15 @@ const port = 8080
 app.get('/', (req, res) => {
     var msg;
     var app_msg = process.env.APP_MSG;
+    var name= process.env.USER_NAME;
+    var user_password= process.env.USER_PASSWORD;
     if (app_msg == "") {
         msg = 'Hello, ' + req.hostname + '. Time is ' + Date() + '.\nYou have no message for me.\n';
     } else {
         msg = 'Hello, ' + req.hostname + '. Time is ' + Date() + '.\nI will deliver your message: ' + app_msg + '\n';
+    }
+    if (name != "" && user_password != ""){
+        msg = 'Your user name is :- ' + name + ' and password is :- ' + user_password + '\n';
     }
     res.send(msg);
 })
