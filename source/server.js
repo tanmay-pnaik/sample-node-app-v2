a8cddea3a1e816b09b16ec3903fd5cefedcf5f49
Gunjan Garge <gunjangarge@gmail.com>
2020-06-07 19:09:58 +0530

    Update server.js

--- source/server.js
+++ source/server.js
@@ -3,11 +3,11 @@ const app = express()
 const port = 8080
 
 app.get('/', (req, res) => {
-    var msg, app_msg;
-    if (app_msg != "") {
+    var msg, APP_MSG;
+    if (APP_MSG == "") {
         msg = 'Hello, ' + req.hostname + '. Time is ' + Date() + '.\nYou have no message for me.\n';
     } else {
-        msg = 'Hello, ' + req.hostname + '. Time is ' + Date() + '.\nI will deliver your message: ' + app_msg + '\n';
+        msg = 'Hello, ' + req.hostname + '. Time is ' + Date() + '.\nI will deliver your message: ' + APP_MSG + '\n';
     }
     res.send(msg);
 })
