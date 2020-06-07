eb677795d6898e54f9bbb8963a2b99bb5823f27e
Gunjan Garge <gunjangarge@gmail.com>
2020-06-07 20:46:07 +0530

    Update server.js

--- source/server.js
+++ source/server.js
@@ -7,12 +7,12 @@ app.get('/', (req, res) => {
     var app_msg = process.env.APP_MSG;
     var name= process.env.USER_NAME;
     var user_password= process.env.USER_PASSWORD;
-    if (app_msg == "") {
+    if (app_msg == undefined) {
         msg = 'Hello, ' + req.hostname + '. Time is ' + Date() + '.\nYou have no message for me.\n';
     } else {
         msg = 'Hello, ' + req.hostname + '. Time is ' + Date() + '.\nI will deliver your message: ' + app_msg + '\n';
     }
-    if (name != "" && user_password != ""){
+    if (name != undefined && user_password != undefined){
         msg = msg + '\nYour user name is :- ' + name + ' and password is :- ' + user_password + '\n';
     }
     res.send(msg);
